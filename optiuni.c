b79ee61f56efd6dae9ee45de7df1297a90d2f902
Sebastian Badea <sebastian.badea0506@stud.acs.upb.ro>
2022-05-27 03:51:48 +0300

    [asd]

--- optiuni.c
+++ optiuni.c
@@ -63,7 +63,97 @@ void afisarecomanda(Lista_Comenzi *Lista_Comenzi, int nrcomanda)
     }
 }
 
-void adaugarecomanda(Lista_Comenzi **listacomanda)
+Meniu *init_meniu(FILE *f)
 {
+    Meniu *meniu = (Meniu *)malloc(sizeof(Meniu));
+    fscanf(f, "%d\n", &meniu->numar_articole);
+    meniu->articole = NULL;
+    Articol *prev = NULL;
+    for (int i = meniu->numar_articole; i > 0; i--)
+    {
+        Articol *articol = malloc(sizeof(Articol));
+        articol->nume = malloc(NR_MAX);
+        fscanf(f, "%s ", articol->nume);
+        fscanf(f, "%d ", &articol->pret);
+        fscanf(f, "%d\n", &articol->numar);
+        if (prev == NULL)
+        {
+            meniu->articole = articol;
+        }
+        else
+        {
+            prev->next = articol;
+        }
+        prev = articol;
+    }
+    prev->next = NULL;
+    return meniu;
+}
+
+void afisaremeniu(Meniu *meniu)
+{
+    Articol *articol = meniu->articole;
+    for (int i = meniu->numar_articole; i > 0; i--)
+    {
+        printf("%s ", articol->nume);
+        printf("%d ", articol->pret);
+        printf("%d\n", articol->numar);
+        articol = articol->next;
+    }
+}
+void afisarecomanda(Lista_Comenzi *Lista_Comenzi, int nrcomanda)
+{
+    if (nrcomanda > Lista_Comenzi->nr_comenzi)
+        return 0;
+    for (int i = 0; i < nrcomanda; i++)
+    {
+
+        Comanda *aux = Lista_Comenzi->Comanda;
+    }
+}
+
+void adaugarecomanda(Lista_Comenzi** listacomanda){
     (*listacomanda)->nr_comenzi++;
+    Comanda *aux = (*listacomanda)->Comanda;
+    while(aux->next != NULL){
+        aux = aux->next;
+    }
+    Comanda *newcommand = plasare();
+    aux->next = newcommand;
+
+    
+}
+
+Comanda *plasare(){
+    
+    Comanda *newcommand = (Comanda *) malloc(sizeof(Comanda));
+    newcommand->status = 0;
+    newcommand->pret = 0;
+    newcommand->articole = NULL;
+    return newcommand;
+}
+
+void adaugarearticol(Comanda **comanda, int k, Meniu* meniu){
+    Articol *articol = meniu->articole;
+    while(k > 0){
+        articol = articol->next;
+
+    }
+    Articol *newarticol =(Articol *) malloc(sizeof(Articol));
+    newarticol->pret = articol->pret;
+    strcpy(newarticol->nume, articol->nume);
+    newarticol->numar = articol->numar;
+   // int counter = 0;
+
+    Articol *finalarticole = (*comanda)->articole;
+    while(finalarticole->next != NULL){
+        finalarticole = finalarticole->next;
+    //    counter++;
+    }
+    finalarticole->next = newarticol;
+    (*comanda)->pret = (*comanda)->pret + newarticol->pret;
+    
+    
+
+
 }
